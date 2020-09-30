d323e3f15c514b46ce1c4b2d58151efadb8cb426
Tneebs <tedneben@gmail.com>
2020-09-30 16:34:58 -0500

    Cards flip and display information

--- toughflix-frontend/src/MovieCard.js
+++ toughflix-frontend/src/MovieCard.js
@@ -1,38 +1,64 @@
 import React from 'react';
 import { Card, Image, Button } from 'semantic-ui-react'
 
-const MovieCard = props => {
-return(
-    <Card color='black'>
-    <Image src={props.movie.poster} wrapped ui={false}/>
-    <Card.Content>
-      <Card.Header>{props.movie.title}</Card.Header>
+class MovieCard extends React.Component {
+
+  state = {
+    isActive: false
+  }
+
+  toggleDetails = () => {
+    this.setState(previousState => {
+      return {
+        isActive: !previousState.isActive}
+      })
+    
+  }
+
+  render(){
+
+    let card1 = (
+      <Card color='black' onClick={() => this.toggleDetails()}>
+    <Image src={this.props.movie.poster} wrapped ui={false}/>
+    </Card>
+    )
+
+    let card2 = (
+      <Card color='black' onClick={() => this.toggleDetails()}>
+      <Card.Content>
       <Card.Meta textAlign='right'>
-        <span className='date'>{props.movie.year}</span>
+        <span className='date'>{this.props.movie.year}</span>
       </Card.Meta>
       <Card.Description>
-        <strong>Cast:</strong> {props.movie.actors}
+        <strong>Cast:</strong> {this.props.movie.actors}
         <br/>
-        <strong>Director:</strong> {props.movie.director}
+        <strong>Director:</strong> {this.props.movie.director}
         <br/>
         <br/>
-        {props.movie.plot}
+        {this.props.movie.plot}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
         <span>
-            <strong>{props.movie.genre}</strong>
+            <strong>{this.props.movie.genre}</strong>
             ▪️
-            <strong>{props.movie.rated}</strong>
+            <strong>{this.props.movie.rated}</strong>
             ▪️
-            <strong>{props.movie.runtime}</strong>
+            <strong>{this.props.movie.runtime}</strong>
         </span>
     </Card.Content>
-    {props.removeBtn ? <Button color='youtube' onClick={() => props.handleList(props.movie)}>Remove from List</Button> : <Button color='youtube' onClick={() => props.handleList(props.movie)}>Add to List</Button>} 
-    
+    {this.props.removeBtn 
+    ? 
+    <Button color='youtube' onClick={() => this.props.handleList(this.props.movie)}>Remove from List</Button> 
+    : 
+    <Button color='youtube' onClick={() => this.props.handleList(this.props.movie)}>Add to List</Button>}
+    </Card>
+    )
 
-  </Card>
+return(
+  !this.state.isActive ? card1 : card2
 )
+}
 }
 export default MovieCard;
 
